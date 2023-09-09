63f610dc45e8955fb4a2377afdfa64b37afc032b
William Almeida <will.allmeida@gmail.com>
2023-09-09 20:27:58 -0300

    Add project files.

---
+++ Helpers/BracketHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Helpers
+{
+    public class BracketHelper
+    {
+        public static bool IsBalanced(string input)
+        {
+            Stack<char> result = new Stack<char>();
+
+            foreach (char c in input)
+            {
+                if (c == '{' || c == '[' || c == '(')
+                {
+                    result.Push(c);
+                }
+                else if (c == '}' || c == ']' || c == ')')
+                {
+                    if (result.Count <= 0)
+                    {
+                        return false;
+                    }
+
+                    char top = result.Pop();
+
+                    if ((c == ')' && top != '(') ||
+                    (c == ']' && top != '[') ||
+                    (c == '}' && top != '{'))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return result.Count == 0 ? true : false;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Application.Helpers;
+
+var input = Console.ReadLine();
+
+if(input is null)
+{
+    Console.WriteLine("Dados inseridos não são válidos");
+    return;
+}
+
+Console.WriteLine(BracketHelper.IsBalanced(input!) ? "Válida" : "Inválida");
