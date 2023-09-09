05fd1c08dabcef7bfda2b6dbbb51a6fa0c1a0bba
William Almeida <will.allmeida@gmail.com>
2023-09-09 20:55:50 -0300

    Allow to pass arguments through command line

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,8 +1,12 @@
 ﻿using Application.Helpers;
 
-var input = Console.ReadLine();
+var argument = Environment.GetCommandLineArgs();
 
-if(input is null)
+Console.WriteLine("Insira uma string:");
+
+var input = argument.Length > 1 ? argument[1] : Console.ReadLine();
+
+if (input is null)
 {
     Console.WriteLine("Dados inseridos não são válidos");
     return;
